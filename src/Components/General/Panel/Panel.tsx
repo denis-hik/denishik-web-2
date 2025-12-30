a8322531edfe22e6973c32653099d0a59cf30e64
Денис Тюшев <dtyushev2015@gmail.com>
2025-12-30 15:29:49 +0300

    scroll wait

--- src/Components/General/Panel/Panel.tsx
+++ src/Components/General/Panel/Panel.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo} from "react";
+import React, {useEffect, useMemo} from "react";
 import {PanelStyled} from "./styled";
 import GlassSurface from "../../public/GlassSurface/GlassSurface";
 import {routes} from "../defaults";
@@ -10,6 +10,25 @@ export const Panel: React.FunctionComponent = () => {
     const active = useMemo(() => {
         return !!routes.find((item) => location.pathname === item.path && !item.hiddenNavBar)
     },[location.pathname])
+
+    useEffect(() => {
+        if (!active)
+            return
+
+        const timer = setTimeout(() => {
+            window.scrollTo({ top: document.documentElement.scrollHeight * 0.1, left: 0, behavior: "smooth" })
+        }, 2000)
+
+        const onScroll = () => {
+            timer && clearTimeout(timer)
+        }
+
+        window.addEventListener("scroll",onScroll)
+        return () => {
+
+            window.removeEventListener("scroll", onScroll)
+        }
+    }, [active]);
 
     return (
         <PanelStyled className={active ? 'active' : ''}>
