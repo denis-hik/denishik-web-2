2eeba269557f12d2e031c2cfb313da1a6be2acb7
tda <tda@weblock.su>
2026-01-27 08:56:41 +0300

    more #3

--- src/pages/ProjectMore/ProjectMorePage.tsx
+++ src/pages/ProjectMore/ProjectMorePage.tsx
@@ -1,13 +1,14 @@
 import {LayeredParallaxBackground} from "../../components/public/BachgroundParalax/BackgroundParalax";
 import React from "react";
 import {ProjectsMorePageStyled} from "./styled";
+import img from "../../media/projectsMore.gif"
 import {ProjectsMorePanel} from "./Components/Panel/ProjectMorePanel";
 
 export const ProjectMorePage = () => {
 
     return (<ProjectsMorePageStyled>
         <LayeredParallaxBackground
-            overlayImageSrc={"https://github.com/denis-hik/SC-Android/blob/master/screens/photo0.jpg?raw=true"}
+            overlayImageSrc={img}
             maxOffset={20}
             overlaySize={"cover"}
             overlayWidth={"120%"}
